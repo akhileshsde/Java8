2286f120cedffc3c1b0eb2ed9052a63490ece94a
Akhilesh Kumar Bhagat <akhileshcoder.ac@gmail.com>
2024-05-21 19:18:16 +0530

    Demo7 Updated

--- Optional Classess/src/com/meteoricmind/optional/demos/Demo7.java
+++ Optional Classess/src/com/meteoricmind/optional/demos/Demo7.java
@@ -8,21 +8,21 @@ public class Demo7 {
 		// Case-1
 		String str1 = null;
 		Optional<String> myOpts1 = Optional.ofNullable(str1);
-		// System.out.println("Optional Object-1 : " + myOpts1);
+		System.out.println("Optional Object-1 : " + myOpts1);
 		Optional<String> myOpts2 = myOpts1.filter(input -> input.contains("Akhil"));
 		System.out.println("Optional Object-2 : " + myOpts2);
 
 		// Case-2
 		String str2 = "Akhilesh";
 		Optional<String> myOpts3 = Optional.ofNullable(str2);
-		// System.out.println("Optional Object-3 : " + myOpts3);
+		System.out.println("Optional Object-3 : " + myOpts3);
 		Optional<String> myOpts4 = myOpts3.filter(input -> input.contains("Akhil"));
 		System.out.println("Optional Object-4 : " + myOpts4);
 
 		// Case-3
 		String str3 = "Hello Guys!!!";
 		Optional<String> myOpts5 = Optional.ofNullable(str3);
-//		System.out.println("Optional Object-5 : " + myOpts5);
+		System.out.println("Optional Object-5 : " + myOpts5);
 		Optional<String> myOpts6 = myOpts5.filter(input -> input.contains("Akhil"));
 		System.out.println("Optional Object-6 : " + myOpts6);
 		
