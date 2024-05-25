0b682a73ee1190c2e4f15d156e91dd7faed7d592
Akhilesh Kumar Bhagat <akhileshcoder.ac@gmail.com>
2024-05-25 17:35:14 +0530

    Updated

--- Streams/src/com/meteoricmind/stream/Demo9.java
+++ Streams/src/com/meteoricmind/stream/Demo9.java
@@ -12,12 +12,14 @@ public class Demo9 {
 		System.out.println("------2------");
 		Stream<Integer> myStream2 = Stream.of(11, 12, 13);
 		myStream2.forEach(System.out::println);
-		List<Integer> numsList = Arrays.asList(11, 12, 13, 14, 15);
 		System.out.println("-----3------");
-		Stream<Integer> myStream3 = numsList.stream();
+		List<Integer> numsList = Arrays.asList(11, 12, 13, 14, 15);
+		Stream<Integer> myStream3 = numsList.stream();//stream is instance number
 		myStream3.forEach(System.out::println);
 		System.out.println("------4-------");
 		Stream<Integer> myStream4 = numsList.parallelStream();
 		myStream4.forEach(System.out::println);
+
+		
 	}
 }
