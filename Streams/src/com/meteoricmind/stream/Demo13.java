85bfb82a03bfbf071810a9e35935733c42b30300
Akhilesh Kumar Bhagat <akhileshcoder.ac@gmail.com>
2024-05-26 12:32:03 +0530

    Generate method implemented

---
+++ Streams/src/com/meteoricmind/stream/Demo13.java
@@ -0,0 +1,25 @@
+package com.meteoricmind.stream;
+
+import java.util.Random;
+import java.util.stream.Stream;
+
+public class Demo13 {
+	public static void main(String[] args) {
+
+		
+		Stream<Integer> myStream = Stream.generate(() -> (new Random()).nextInt(100));
+		myStream.limit(10).forEach(System.out::println);
+		System.out.println("---------------------------------");
+		Stream.generate(() -> (new Random()).nextInt(100)).limit(10).filter(num -> num % 2 == 0)
+				.forEach(System.out::println);
+		System.out.println("---------------------------------");
+		Stream.generate(() -> (new Random()).nextInt(100)).limit(10).filter(num -> num % 2 != 0)
+				.forEach(System.out::println);
+		System.out.println("---------------------------------");
+		Stream.generate(() -> (new Random()).nextInt(10)).limit(10).filter(num -> num % 2 != 0).map(num->num*num)
+				.forEach(System.out::println);
+		System.out.println("---------------------------------");
+		Stream.generate(() -> (new Random()).nextInt(10)).limit(10).filter(num -> num % 2 == 0).map(num->num*num)
+				.forEach(System.out::println);
+	}
+}
