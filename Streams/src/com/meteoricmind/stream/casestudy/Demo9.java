e0573be153482d12e4d3415d6ed592e9819a1e2f
Akhilesh Kumar Bhagat <akhileshcoder.ac@gmail.com>
2024-05-27 23:05:32 +0530

    Partitioning by implemented

---
+++ Streams/src/com/meteoricmind/stream/casestudy/Demo9.java
@@ -0,0 +1,25 @@
+package com.meteoricmind.stream.casestudy;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Demo9 {
+	public static void main(String[] args) {
+		// Display the students group by course name
+		List<Student> studentList = DataUtil.getStudentList();
+		Map<Boolean, List<Student>> stuMap = studentList.stream()
+				.collect(Collectors.partitioningBy(stu -> stu.getFeeBal() == 0));
+		for (Boolean flag : stuMap.keySet()) {
+			List<Student> stuList = stuMap.get(flag);
+			if (flag) {
+				System.out.println("Students with no fee balance");
+			} else {
+				System.out.println("Students with  fee balance");
+			}
+			stuList.forEach(System.out::println);
+			System.out.println("-----------------------------");
+		}
+
+	}
+}
