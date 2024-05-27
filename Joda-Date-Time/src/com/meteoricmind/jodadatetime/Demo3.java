05aefaf9598b525d6301e6fa6e705bccf1948d1e
Akhilesh Kumar Bhagat <akhileshcoder.ac@gmail.com>
2024-05-28 00:00:16 +0530

    Explore

---
+++ Joda-Date-Time/src/com/meteoricmind/jodadatetime/Demo3.java
@@ -0,0 +1,13 @@
+package com.meteoricmind.jodadatetime;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class Demo3 {
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.now();
+		System.out.println(date);
+		 LocalTime time=LocalTime.now();
+		 System.out.println(time);
+		 } 
+}
