44b0eec8e7ae075d56081e8a00e472d537447f42
Akhilesh Kumar Bhagat <akhileshcoder.ac@gmail.com>
2024-05-31 23:54:43 +0530

    Demo18 added

--- Streams/src/com/meteoricmind/deepdiveonstream/Demo18.java
+++ Streams/src/com/meteoricmind/deepdiveonstream/Demo18.java
@@ -1,5 +1,18 @@
 package com.meteoricmind.deepdiveonstream;
 
-public class Demo18 {
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+public class Demo18 {
+	public static void main(String[] args) {
+		// Q18) What is the Total Fee Paid till now by Course-wise?
+
+		List<Student> studentList = DataUtil.getStudentList();
+		Map<String, Double> feePaidMap = studentList.stream()
+				.collect(Collectors.groupingBy(Student::getCourseName, Collectors.summingDouble(Student::getFeePaid)));
+		for (String courseName : feePaidMap.keySet()) {
+			System.out.println(courseName + " : " + feePaidMap.get(courseName));
+		}
+	}
 }
