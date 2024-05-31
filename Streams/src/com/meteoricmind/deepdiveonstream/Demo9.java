2c9a007696ac2f161f3d94b588f32251cc7b2327
Akhilesh Kumar Bhagat <akhileshcoder.ac@gmail.com>
2024-05-31 23:46:52 +0530

    Demo9 Added

---
+++ Streams/src/com/meteoricmind/deepdiveonstream/Demo9.java
@@ -0,0 +1,15 @@
+package com.meteoricmind.deepdiveonstream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Demo9 {
+	public static void main(String[] args) {
+		// Q9) How Many Students has to pay the fee bal from DevOps Course
+		List<Student> studentList = DataUtil.getStudentList();
+		System.out.println("--------------------------");
+		long count1 = studentList.stream().filter(mystu -> mystu.getCourseName().equals("DevOps"))
+				.filter(mystu -> mystu.getFeeBal() > 0).collect(Collectors.counting());
+		System.out.println(count1);
+	}
+}
