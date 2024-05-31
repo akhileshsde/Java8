b1d07f8c2eccff3655b9c7532b0a7221f23a0702
Akhilesh Kumar Bhagat <akhileshcoder.ac@gmail.com>
2024-05-31 23:49:34 +0530

    Demo12

---
+++ Streams/src/com/meteoricmind/deepdiveonstream/Demo12.java
@@ -0,0 +1,19 @@
+package com.meteoricmind.deepdiveonstream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Demo12 {
+	public static void main(String[] args) {
+		//Q12) Which Student Fee bal is more (max) 
+
+		List<Student> studentList = DataUtil.getStudentList();
+		Optional<Student> studentOpts3 = studentList.stream()
+				.max((stu1, stu2) -> (int) (stu1.getFeeBal() - stu2.getFeeBal()));
+		studentOpts3.ifPresent(System.out::println);
+		System.out.println("--------------------------");
+		Optional<Student> stuOpts1 = studentList.stream()
+				.collect(Collectors.maxBy((stu1, stu2) -> (int) (stu1.getFeeBal() - stu2.getFeeBal())));
+		stuOpts1.ifPresent(System.out::println);
+	}
+}
