98e342f8d974583765af773745c1d157c586783f
Akhilesh Kumar Bhagat <akhileshcoder.ac@gmail.com>
2024-05-31 23:48:45 +0530

    Demo11 added

---
+++ Streams/src/com/meteoricmind/deepdiveonstream/Demo11.java
@@ -0,0 +1,20 @@
+
+package com.meteoricmind.deepdiveonstream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Demo11 {
+	public static void main(String[] args) {
+		//Q11) Which Student has Paid Fee less (min) 
+
+		List<Student> studentList = DataUtil.getStudentList();
+		Optional<Student> studentOpts2 = studentList.stream()
+				.min((stu1, stu2) -> (int) (stu1.getFeePaid() - stu2.getFeePaid()));
+		studentOpts2.ifPresent(System.out::println);
+		System.out.println("--------------------------");
+		Optional<Student> stuOpts2 = studentList.stream()
+				.collect(Collectors.minBy((stu1, stu2) -> (int) (stu1.getFeePaid() - stu2.getFeePaid())));
+		stuOpts2.ifPresent(System.out::println);
+	}
+}
