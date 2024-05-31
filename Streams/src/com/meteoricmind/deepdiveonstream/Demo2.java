5d3bc018cd7e96c5c24d8f4481d9d1eebc79fe6b
Akhilesh Kumar Bhagat <akhileshcoder.ac@gmail.com>
2024-05-31 23:40:37 +0530

    Demo 2 and 3 added

---
+++ Streams/src/com/meteoricmind/deepdiveonstream/Demo2.java
@@ -0,0 +1,16 @@
+package com.meteoricmind.deepdiveonstream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Demo2 {
+	public static void main(String[] args) {
+		List<Student> studentList = DataUtil.getStudentList();
+		studentList.stream().filter(mystu -> mystu.getFeeBal() > 0).forEach(System.out::println);
+		System.out.println("--------------------------");
+		List<Student> mylist1 = studentList.stream().filter(mystu -> mystu.getFeeBal() > 0)
+				.collect(Collectors.toList());
+		mylist1.forEach(System.out::println);
+		System.out.println("--------------------------");
+	}
+}
---
+++ Streams/src/com/meteoricmind/deepdiveonstream/Demo3.java
@@ -0,0 +1,16 @@
+package com.meteoricmind.deepdiveonstream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Demo3 {
+	public static void main(String[] args) {
+		List<Student> studentList = DataUtil.getStudentList();
+		studentList.stream().sorted((stu1, stu2) -> stu1.getSname().compareTo(stu2.getSname()))
+				.forEach(System.out::println);
+		System.out.println("--------------------------");
+		List<Student> mylist = studentList.stream().sorted((stu1, stu2) -> stu2.getSname().compareTo(stu1.getSname()))
+				.collect(Collectors.toList());
+		mylist.forEach(System.out::println);
+	}
+}
