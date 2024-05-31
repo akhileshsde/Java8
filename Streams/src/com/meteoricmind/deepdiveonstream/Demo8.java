404b7ae19a48015f82bf939f12684c48eb0b4926
Akhilesh Kumar Bhagat <akhileshcoder.ac@gmail.com>
2024-05-31 23:45:58 +0530

    Demo8 Added

---
+++ Streams/src/com/meteoricmind/deepdiveonstream/Demo8.java
@@ -0,0 +1,14 @@
+package com.meteoricmind.deepdiveonstream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Demo8 {
+	public static void main(String[] args) {
+		// Q8) Display the Students who joined for DevOps Course in DESC Order by Name
+		List<Student> studentList = DataUtil.getStudentList();
+		List<Student> mylist = studentList.stream().filter(mystu -> mystu.getCourseName().equals("DevOps"))
+				.sorted((stu1, stu2) -> stu2.getSname().compareTo(stu1.getSname())).collect(Collectors.toList());
+		mylist.forEach(System.out::println);
+	}
+}
