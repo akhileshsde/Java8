6f23195f086fbdd16782b1f31f45db4baaa4b9f2
Akhilesh Kumar Bhagat <akhileshcoder.ac@gmail.com>
2024-05-31 23:51:26 +0530

    Demo14 added

---
+++ Streams/src/com/meteoricmind/deepdiveonstream/Demo14.java
@@ -0,0 +1,23 @@
+package com.meteoricmind.deepdiveonstream;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Demo14 {
+	public static void main(String[] args) {
+		//Q14) Display the Students who has to pay the Bal and Who Paid separately. 
+
+		List<Student> studentList = DataUtil.getStudentList();
+		Map<Boolean, List<Student>> stuMap = studentList.stream()
+				.collect(Collectors.partitioningBy(stu -> stu.getFeeBal() == 0));
+		for (boolean flag : stuMap.keySet()) {
+			List<Student> mylist = stuMap.get(flag);
+			if (flag) {
+				System.out.println("Students with NO Bal: ");
+			} else {
+				System.out.println("Students with Bal to Pay: ");
+			}
+			mylist.forEach(System.out::println);
+		}
+	}
+}
